f74989c0ab2f8579db32a32b9f87b4e9e2e9b059
Hustada <hustada80@gmail.com>
2014-06-15 14:42:17 -0500

    adding new users to application

--- routes/auth.js
+++ routes/auth.js
@@ -10,3 +10,16 @@ router.get('/register', function(req, res) {
   res.render('register', {title: 'Register', error: req.flash('error')[0]
 });
 });
+
+// Register a new user to Stormpath
+router.post('/resister, function'(req, res){
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  // Grab user fields:
+
+  if (!username || !password) {
+    return res.render('register', {title: 'Register', error: 'Email and password required.'});
+  }
+});
